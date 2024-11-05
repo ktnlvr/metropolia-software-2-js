f175b843f2035be530c0e22520a624d283ed8f1b
ktnlvr <artur.roos.artur@gmail.com>
2024-11-05 22:27:31 +0000

    Fix prime checker

--- module01/assignment09.js
+++ module01/assignment09.js
@@ -1,9 +1,11 @@
 function isPrime(n) {
-    for (let i = 2; i < Math.sqrt(n); i++)
-        if (n % i == 0)
-            return false;
-    return true;
+    for (let i = 2; i < Math.ceil(Math.round(n)); i++)
+        if (n % i == 0) {
+            console.log(n)
+            return true;
+        }
+    return n < 1;
 }
 
 let n = parseInt(prompt("Number?"))
-document.querySelector("#output").textContent = `${n} is ` + ["", "not "][+!isPrime(n)] + "prime"
+document.querySelector("#output").textContent = `${n} is ` + ["", "not "][+isPrime(n)] + "prime"
