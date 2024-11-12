53ece3bd8dc53462cba348bf870e59555a22806c
ktnlvr <artur.roos.artur@gmail.com>
2024-11-12 14:06:59 +0000

    Fix up some minor bugs in part 1

--- module01/assignment06.js
+++ module01/assignment06.js
@@ -1,10 +1,11 @@
 let out = document.querySelector("#output")
 if (confirm("Should I calculate the square root?")) {
-    let input = parseInt(prompt("Enter a number"));
+    let input = +prompt("Enter a number");
     if (input < 0) {
         out.textContent = "Can't take a square root of a negative number";
-        return;
+    } else {
+        out.textContent = Math.sqrt(input);
     }
-
-    out.textContent = Math.sqrt(input);
+} else {
+    out.textContent = "The square root is not calculated."
 }
--- module01/assignment07.js
+++ module01/assignment07.js
@@ -1,6 +1,5 @@
-let dice_count = parseInt(prompt("How many dice to roll?"))
+let dice_count = +prompt("How many dice to roll?")
 let dice_sum = 0
 for (let i = 0; i < dice_count; i++)
-    dice_sum += Math.round(Math.random() * 6) + 1;
-
+    dice_sum += Math.floor(Math.random() * 5) + 1;
 console.log(`Dice total for ${dice_count}d6: ${dice_sum}`)
